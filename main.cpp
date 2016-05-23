bff4c6f59780bdb45046cea13568a867d6fe40b1
fzf <fanzhifangfzf@gmail.com>
2016-05-23 11:15:55 +0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QRcodeCPP
+//
+//  Created by 范志方 on 16/5/23.
+//  Copyright © 2016年 范志方. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
